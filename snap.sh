a2a91f7fbef74636a47144e3716de65b5fc1df5a
Marc Oggier <marc.oggier@gmail.com>
2013-12-08 12:46:42 -0900

    User need to specifiy directory where the pictures will be stored
    Signed-off-by: Marc Oggier <marc.oggier@gmail.com>

--- snap.sh
+++ snap.sh
@@ -1,7 +1,20 @@
 #!/bin/bash
- 
+#
+# SnowPI
+# 
+# Marc Oggier
+# Simon Filhol
+#
+# Updated December 9, 2013
+# Version 2.1
+#
+## Man
+# snap.sh [DIR]
+
 APP=/usr/bin/uvccapture
 TIMESTAMP=$(date +%s)
+DIR=$1
+
 $APP -B20 -C39 -S50 -v -x1600 -y1200 -otemp.jpg
 
 TIMEH=$(date +"%Y-%m-%d-%H_%M" -d @$TIMESTAMP)
@@ -9,6 +22,6 @@ TIMEH=$(date +"%Y-%m-%d-%H_%M" -d @$TIMESTAMP)
 FILENAME=snow-$TIMEH.jpg
 FILE=$FILENAME
 cp temp.jpg current.jpg
-mv temp.jpg ./pics/$FILE
+mv temp.jpg $DIR/$FILE
  
-echo -e "$FILENAME \t $TIMESTAMP \t jpg" > temp.txt
+echo -e "$FILENAME \t $TIMESTAMP \t jpg" > $DIR/temp.txt
