b95f5c0ddbac5487a71167f46281666f1736eef7
root <root@localhost.localdomain>
2013-12-08 13:29:17 -0900

    minor modification
    Signed-off-by: root <root@localhost.localdomain>

--- capture.py
+++ capture.py
@@ -14,6 +14,9 @@
 import serial
 import time
 import os
+
+dir='/mnt/data/test'
+
 #locations=['/dev/ttyACM0','/dev/ttyACM1','/dev/ttyACM2','/dev/ttyACM3', '/dev/ttyS0','/dev/ttyS1','/dev/ttyS2','/dev/ttyS3'] 
 #for device in locations:device
 #    try:
@@ -23,14 +26,14 @@ import os
 fl = open("log.log", "a")
 #arduino = serial.Serial('/dev/ttyAMA0',9600)
 arduino = serial.Serial('/dev/ttyACM0',9600)
-time.sleep(1)
+time.sleep(2)
 arduino.write('u')
 fl.write(time.strftime("%Y-%m-%d %H:%M:%S", time.localtime()) +"\tUSB ON\n")
 time.sleep(5)
 arduino.write('y')
 fl.write(time.strftime("%Y-%m-%d %H:%M:%S", time.localtime()) +"\tLED ON\n")
 time.sleep(1)
-os.system("./snap.sh /mnt/data/pics")
+os.system("./snap.sh "+dir)
 fl.write(time.strftime("%Y-%m-%d %H:%M:%S", time.localtime()) +"\ttaking picture\n")
 time.sleep(5)
 arduino.write('d');
@@ -51,12 +54,12 @@ t2=t2[0:len(t2)-2];
 t3=t3[0:len(t3)-2];
 t4=t4[0:len(t4)-2];
 t5=t5[0:len(t5)-2];
-f = open("temp.txt", "r")
+f = open(dir+"/temp.txt", "r")
 temp=f.readline()
 temp=temp[0:len(temp)-1]
 f.close()
 temp= str(temp) + "\t" + str(float(t1)/100) +"\t"+str(float(t2)/100)+"\t"+str(float(t3)/100)+"\t"+str(float(t4)/100)+"\t"+ str(float(t5)/100)+"\n"
-f = open("log.txt", "a")
+f = open(dir+"/log.txt", "a")
 f.write(temp)
 f.close()
 # Closing log file
